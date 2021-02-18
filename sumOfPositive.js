3887f0b85c32b9fc6022db6e409a730bca0b3907
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-18 10:37:38 -0500

    fixed array length expression. Originally used Python syntax

--- sumOfPositive.js
+++ sumOfPositive.js
@@ -6,7 +6,7 @@ function positiveSum(arr) {
 
     let arrSum = 0
 
-    for (let i = 0; i = len(arr)-1; i++){
+    for (let i = 0; i = arr.length-1; i++){
     	if (arr[i] > 0) {
         	arrSum += arr[i]
         	console.log arrSum
