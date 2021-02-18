34bb708465738b03b3fbf87b3017d894ed647488
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-18 10:32:09 -0500

    Copied current work from CodeWars site to use VS Code UI to visually assist coding

---
+++ sumOfPositive.js
@@ -0,0 +1,20 @@
+//You get an array of numbers, return the sum of all of the positives ones.
+//Example [1,-4,7,12] => 1 + 7 + 12 = 20
+//Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+
+    let arrSum = 0
+
+    for (let i = 0; i = len(arr)-1; i++){
+    	if (arr[i] > 0) {
+        	arrSum += arr[i]
+        	console.log arrSum
+      	}
+      	else {
+        	console.log arrSum
+      	}
+      	return arrSum
+    }
+  //return 
+}	
