df73007722383361fa322dd2176c58e87913a195
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-21 12:34:47 -0500

    added the logic used in previous commit into copde wars Class. tested. Passed all tests. Copied and replaced class here

--- smallestInt.js
+++ smallestInt.js
@@ -6,16 +6,15 @@
 // Given [34, -345, -1, 100] your solution will return -345
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
+class SmallestIntegerFinder {
+	findSmallestInt(args) {
+		var small = args[0];
 
-function findSmallestInt(args) {
-	var small = args[0];
-
-	for (let i = 0; i < args.length; i++) {
-		if (args[i] < small) {
-			small = args[i];
+		for (let i = 0; i < args.length; i++) {
+			if (args[i] < small) {
+				small = args[i];
+			}
 		}
+		return small;
 	}
-	return small;
 }
-test = findSmallestInt([34, -345, -1, 100]);
-test;
