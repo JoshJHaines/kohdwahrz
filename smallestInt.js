724d48ac012f1ce58bfbbc0ddcb0c88751f6613b
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-21 12:32:32 -0500

    removed Class to test logic of the function. Probably broke the whole thing.

--- smallestInt.js
+++ smallestInt.js
@@ -6,17 +6,16 @@
 // Given [34, -345, -1, 100] your solution will return -345
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
-class SmallestIntegerFinder {
-    findSmallestInt(args) {
-        var small = 0;
 
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] < small) {
-                small = arr[i];
-            }
-        }
-        return small;
-    }
-  }
+function findSmallestInt(args) {
+	var small = args[0];
 
-  
+	for (let i = 0; i < args.length; i++) {
+		if (args[i] < small) {
+			small = args[i];
+		}
+	}
+	return small;
+}
+test = findSmallestInt([34, -345, -1, 100]);
+test;
