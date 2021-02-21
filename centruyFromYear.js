a3c896a2c74b4455cb7031f890b84a9e7bad30cd
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-21 13:11:15 -0500

    Used Splice to convert all years into 4 digit arays

--- centruyFromYear.js
+++ centruyFromYear.js
@@ -8,8 +8,30 @@
 //centuryFromYear(1900)  returns (19)
 //centuryFromYear(1601)  returns (17)
 //centuryFromYear(2000)  returns (20)
+//centuryFromYear(89) returns (1)
+
+//turn year into array
+//make all arrays 4 digits
+//make first two index one value
+//make second two index another value
+//if second index is less than 1, return first index
+//if second index is greater than or equal to 1, return first index +1
 
 function century(year) {
-    // Finish this :)
-    return;
-  }
+	const turnToArr = year;
+	const arrayYear = Array.from(String(turnToArr), Number);
+	if (arrayYear.length < 2) {
+		arrayYear.splice(0, 0, 0);
+	}
+	if (arrayYear.length < 3) {
+		arrayYear.splice(0, 0, 0);
+	}
+	if (arrayYear.length < 4) {
+		arrayYear.splice(0, 0, 0);
+	}
+
+	return arrayYear;
+}
+
+test = century(999);
+test;
