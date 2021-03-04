015bfdb575174a316b11bb0a998ef64eac855205
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-04 12:17:39 -0500

    tested in code wars

---
+++ foldToMoon.js
@@ -0,0 +1,12 @@
+function foldTo(distance) {
+    if (distance <= 0){
+        return null
+    }
+    thickness = 0.0001
+    timesFolded = 0
+    while (thickness <= distance){
+        timesFolded += 1;
+        thickness = thickness * 2
+    }
+    return timesFolded
+}
