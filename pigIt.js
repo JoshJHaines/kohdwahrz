032381b6a5111c87d33e42db39610575cbe081bb
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-22 11:01:54 -0400

    whitespace is the devil

--- pigIt.js
+++ pigIt.js
@@ -5,24 +5,32 @@
 // pigIt('Hello world !');     // elloHay orldway !
 
 function pigIt(str) {
-	firstLetter = ""
-    newStr = ""
-    for (i=0, i < str.length, i++){
-        if (i === 0){
-            firstLetter = str[i]
-        }
-        if (str[i] === ' '){
-            newStr += `${firstLetter} `;
-            firstLetter = str[i + 1];
-            i++
-        } else {
-            newStr += str[i]
-        }
-    }
-console.log(newStr)
+	firstLetter = "";
+	newStr = "";
+	for (i = 0; i < str.length; i++) {
+		if (i === 0) {
+			firstLetter = str[i];
+			newStr = "";
+		}
+		if (str[i] === " ") {
+			newStr += `${firstLetter}ay `;
+			firstLetter = str[i + 1];
+			i++;
+		} else if (i !== 0) {
+			newStr += str[i];
+		}
+	}
+	if (firstLetter === "!" || firstLetter === "?" || firstLetter === ".") {
+		newStr += `${firstLetter}`;
+	} else {
+		newStr += `${firstLetter}ay`;
+	}
+	return newStr;
 }
 
-pigIt('Hello world !')
-console.log(pigIt('Hello world !'))
-
-console.log(6)
+test1 = pigIt("Hello world !");
+test1;
+test2 = pigIt("Pig latin is cool");
+test2;
+test3 = pigIt("This is my string");
+test3;
